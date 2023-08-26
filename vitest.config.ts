f5e4bcfabc0ef9e7b0c97a4336c765ee67949169
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-26 17:01:29 +0900

    chore:add testing-library (#3)

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,12 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+// FIXME: https://github.com/vitest-dev/vitest/discussions/1573#discussioncomment-3653362
 
 export default defineConfig({
-  test: {},
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./tests/setup.ts'],
+  },
 });
