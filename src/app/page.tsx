11d5c4a70b4119caf66b4a8e9c7e5524fa242eeb
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-26 18:50:46 +0900

    chore: add a rule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { css } from '@/styled-system/css';
 
-export default function Home() {
-  return (
-    <div className={css({ fontSize: '6xl', fontWeight: 'bold' })}>Home</div>
-  );
-}
+const Home = () => (
+  <div className={css({ fontSize: '6xl', fontWeight: 'bold' })}>Home</div>
+);
+
+export default Home;
