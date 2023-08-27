2730aa0058f1533997d2bcd0fd0a49765f8a7452
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-27 16:54:25 +0900

    chore: add a rule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,16 +6,17 @@ const Home = ({
 }: {
   readonly isBool: boolean;
   readonly onClick: () => void;
-}) => (
-  <button
-    className={css({ fontSize: '6xl', fontWeight: 'bold' })}
-    onClick={onClick}
-    type="button"
-  >
-    {isBool ? 'Home' : null}
-  </button>
-);
+}) => {
+  const arrowFunctionReturnsPromise = async () => Promise.resolve('value');
+  return (
+    <button
+      className={css({ fontSize: '6xl', fontWeight: 'bold' })}
+      onClick={onClick}
+      type="button"
+    >
+      {isBool ? 'Home' : null}
+    </button>
+  );
+};
 
 export default Home;
-
-const test = [1, 2, 3].reduce<readonly number[]>((arr, num) => arr.concat(num * 2), []);
