abb67b4ab7c46993912df4958d9c98caa95082d6
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-27 15:41:29 +0900

    chore: add a rule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,4 @@
 import { css } from '@/styled-system/css';
-import { useCallback } from 'react';
-
-type Example = {
-  property?: string;
-}
-
-const example: Example = {};
-const isIncludesBaz = example.property?.includes('baz');
 
 const Home = ({
   isBool,
@@ -14,40 +6,17 @@ const Home = ({
 }: {
   isBool: boolean;
   onClick: () => void;
-}) => {
-  function foo(arg: 'bar' | undefined) {
-    if (arg) {
-      console.log(arg);
-    }
-  }
-
-  const doSomething = useCallback(() => {
-    if (!isBool) {
-      console.error('Nothing to do!');
-      return;
-    }
-
-    onClick();
-  }, [isBool, onClick]);
-
-  if (!isBool) {
-    return null;
-  }
-
-  return (
-    <button
-      className={css({ fontSize: '6xl', fontWeight: 'bold' })}
-      onClick={doSomething}
-      type="button"
-    >
-      {isBool ? 'Home' : null}
-    </button>
-  );
-};
+}) => (
+  <button
+    className={css({ fontSize: '6xl', fontWeight: 'bold' })}
+    onClick={onClick}
+    type="button"
+  >
+    {isBool ? 'Home' : null}
+  </button>
+);
 
 export default Home;
 
-enum A {
-  B,
-  C = B,
-}
+function f<T = number>() {}
+f();
