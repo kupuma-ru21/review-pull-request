c1b47cccc32b3a08af1317d6b7e56e7fbf7a23c6
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-27 14:15:05 +0900

    chore: add a rule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 import { css } from '@/styled-system/css';
 
-const Home = () => (
-  <div className={css({ fontSize: '6xl', fontWeight: 'bold' })}>Home</div>
+const Home = ({
+  isBool,
+  onClick,
+}: {
+  isBool: boolean;
+  onClick: () => void;
+}) => (
+  <button
+    className={css({ fontSize: '6xl', fontWeight: 'bold' })}
+    onClick={onClick}
+    type="button"
+  >
+    {isBool ? 'Home' : null}
+  </button>
 );
 
 export default Home;
