334883163fb9ade034992f355887853a656c33e8
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-27 18:22:38 +0900

    chore: revert page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,7 @@
 import { css } from '@/styled-system/css';
 
-const Home = ({
-  isBool,
-  onClick,
-}: {
-  readonly isBool: boolean;
-  readonly onClick: () => void;
-}) => {
-  async function invalidInTryCatch1() {
-    try {
-      return await Promise.resolve('try');
-    } catch (e) {
-      return null;
-    }
-  }
-
-  return (
-    <button
-      className={css({ fontSize: '6xl', fontWeight: 'bold' })}
-      onClick={onClick}
-      type="button"
-    >
-      {isBool ? 'Home' : null}
-    </button>
-  );
-};
+const Home = () => (
+  <div className={css({ fontSize: '6xl', fontWeight: 'bold' })}>Home</div>
+);
 
 export default Home;
