ba3bf5c6a43759d91b0e8d3e709dd2c7f39d5016
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-27 16:05:23 +0900

    chore: add a rule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,24 @@
 import { css } from '@/styled-system/css';
 
-enum Fruit {
-  Apple,
-  Banana
-}
-
 const Home = ({
   isBool,
   onClick,
 }: {
     isBool: boolean;
     onClick: () => void;
-}) => {
-  function foo1() {
-    return 1 as any;
-  }
-
-  return (
-    <button
-      className={css({ fontSize: '6xl', fontWeight: 'bold' })}
-      onClick={onClick}
-      type="button"
-    >
-      {isBool ? 'Home' : null}
-    </button>
-  );
-};
+}) => (
+  <button
+    className={css({ fontSize: '6xl', fontWeight: 'bold' })}
+    onClick={onClick}
+    type="button"
+  >
+    {isBool ? 'Home' : null}
+  </button>
+);
 
 export default Home;
+
+enum Status {
+  Open = 1,
+  Close = 2,
+}
