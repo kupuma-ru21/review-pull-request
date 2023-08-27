e66e56e6d7976a57bf6475bc94918c62888cbc8a
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-27 16:08:05 +0900

    chore: add a rule

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,23 @@ const Home = ({
 }: {
     isBool: boolean;
     onClick: () => void;
-}) => (
-  <button
-    className={css({ fontSize: '6xl', fontWeight: 'bold' })}
-    onClick={onClick}
-    type="button"
-  >
-    {isBool ? 'Home' : null}
-  </button>
-);
+}) => {
+  function foo1() {
+    const array = [1];
+    for (let i = 0; i < array.length; i += 1) {
+      console.log(array[i]);
+    }
+  }
+
+  return (
+    <button
+      className={css({ fontSize: '6xl', fontWeight: 'bold' })}
+      onClick={onClick}
+      type="button"
+    >
+      {isBool ? 'Home' : null}
+    </button>
+  );
+};
 
 export default Home;
-
-enum Status {
-  Open = 1,
-  Close = 2,
-}
