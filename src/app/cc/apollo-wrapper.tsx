f918772dfb3cbfc99b8230ae627f49d8c268b10b
kupuma-ru21 <kupumaru21@gmail.com>
2023-08-30 15:59:33 +0900

    feat: add ApolloWrapper

---
+++ src/app/cc/apollo-wrapper.tsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { ApolloLink, HttpLink } from '@apollo/client';
+import {
+  NextSSRApolloClient,
+  ApolloNextAppProvider,
+  NextSSRInMemoryCache,
+  SSRMultipartLink,
+} from '@apollo/experimental-nextjs-app-support/ssr';
+import { loadErrorMessages, loadDevMessages } from '@apollo/client/dev';
+import { setVerbosity } from 'ts-invariant';
+import type { PropsWithChildren } from 'react';
+
+if (process.env.NODE_ENV === 'development') {
+  setVerbosity('debug');
+  loadDevMessages();
+  loadErrorMessages();
+}
+
+function makeClient() {
+  const httpLink = new HttpLink({
+    uri: 'https://fragrant-shadow-9470.fly.dev/',
+  });
+
+  return new NextSSRApolloClient({
+    cache: new NextSSRInMemoryCache(),
+    link:
+      typeof window === 'undefined'
+        ? ApolloLink.from([
+          // in a SSR environment, if you use multipart features like
+          // @defer, you need to decide how to handle these.
+          // This strips all interfaces with a `@defer` directive from your queries.
+          new SSRMultipartLink({
+            stripDefer: true,
+          }),
+          httpLink,
+        ])
+        : httpLink,
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/prefer-readonly-parameter-types
+const ApolloWrapper = ({ children }: PropsWithChildren) => (
+  <ApolloNextAppProvider makeClient={makeClient}>
+    {children}
+  </ApolloNextAppProvider>
+);
+
+export default ApolloWrapper;
--- src/app/rsc/page.tsx
+++ src/app/rsc/page.tsx
@@ -1,4 +1,5 @@
 import { GetPollDocument } from '@/src/components/poll/documents.generated';
+import ApolloWrapper from '@/src/app/cc/apollo-wrapper';
 import { getClient } from './client';
 import Poll from './poll-rsc';
 
@@ -9,8 +10,10 @@ const Rsc = async () => {
   });
 
   return (
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    <Poll poll={data.data.poll!} />
+    <ApolloWrapper>
+      {/* eslint-disable-next-line @typescript-eslint/no-non-null-assertion */}
+      <Poll poll={data.data.poll!} />
+    </ApolloWrapper>
   );
 };
 
