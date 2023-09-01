181c9a72c6bebdd0def8807a5ed332711b096f88
kupuma-ru21 <kupumaru21@gmail.com>
2023-09-01 19:21:27 +0900

    feat: render todos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import { css } from '@/styled-system/css';
+import { TodosDocument } from './todos.generated';
+import getClient from './client';
 
-const Home = () => (
-  <div className={css({ fontSize: '6xl', fontWeight: 'bold' })}>Home</div>
-);
+const Home = async () => {
+  const { data } = await getClient().query({ query: TodosDocument });
+  return data.todos.map(
+    (todo: { readonly id: string; }) => (
+      <div
+        className={css({ fontSize: '6xl', fontWeight: 'bold' })}
+      >
+        {todo.id}
+      </div>
+    ),
+  );
+};
 
 export default Home;
