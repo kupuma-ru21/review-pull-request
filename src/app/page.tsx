a364571dfaccbae1ec5efdd112e108005146d451
kupuma-ru21 <kupumaru21@gmail.com>
2023-09-07 21:43:54 +0900

    feat: add ApolloWrapper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,20 @@
 import { css } from '@/styled-system/css';
 import getClient from '@/src/client';
 import { TodosDocument } from './todos.generated';
+import ApolloWrapper from './apollo-wrapper';
 
 const Home = async () => {
   const { data } = await getClient().query({ query: TodosDocument });
-  return data.todos.map(
-    (todo: { readonly id: string; }) => (
-      <div
-        className={css({ fontSize: '6xl', fontWeight: 'bold' })}
-      >
-        {todo.id}
-      </div>
-    ),
+  return (
+    <ApolloWrapper>
+      {data.todos.map((todo: { readonly id: string; }) => (
+        <div
+          className={css({ fontSize: '6xl', fontWeight: 'bold' })}
+        >
+          {todo.id}
+        </div>
+      ))}
+    </ApolloWrapper>
   );
 };
 
