afbab4e62d2549523ec1471a62068f60df189edd
kupuma-ru21 <kupumaru21@gmail.com>
2023-09-08 00:34:39 +0900

    Revert "feat: add ApolloWrapper"
    This reverts commit a364571dfaccbae1ec5efdd112e108005146d451.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,17 @@
 import { css } from '@/styled-system/css';
 import getClient from '@/src/client';
 import { TodosDocument } from './todos.generated';
-import ApolloWrapper from './apollo-wrapper';
 
 const Home = async () => {
   const { data } = await getClient().query({ query: TodosDocument });
-  return (
-    <ApolloWrapper>
-      {data.todos.map((todo: { readonly id: string; }) => (
-        <div
-          className={css({ fontSize: '6xl', fontWeight: 'bold' })}
-        >
-          {todo.id}
-        </div>
-      ))}
-    </ApolloWrapper>
+  return data.todos.map(
+    (todo: { readonly id: string; }) => (
+      <div
+        className={css({ fontSize: '6xl', fontWeight: 'bold' })}
+      >
+        {todo.id}
+      </div>
+    ),
   );
 };
 
