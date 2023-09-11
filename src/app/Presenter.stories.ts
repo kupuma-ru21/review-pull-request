ba46ad22c589c9749fd66b0f5109bb6a255ddc99
kupuma-ru21 <kupumaru21@gmail.com>
2023-09-11 21:06:35 +0900

    fix: perspectives to publish

--- src/app/Presenter.stories.ts
+++ src/app/Presenter.stories.ts
@@ -1,7 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
 import Presenter from './components/presenter';
-import { perspectives } from './components/container';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 const meta = {
@@ -12,4 +11,8 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = { args: { perspectives } };
+export const Default: Story = {
+  args: {
+    perspectives: ['perspective1', 'perspective2', 'perspective3'],
+  },
+};
