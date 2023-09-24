b25027e9acb91763c545147811a7544f58547a99
kupuma-ru21 <kupumaru21@gmail.com>
2023-09-24 21:26:23 +0900

    chore: remove unused array

--- src/app/components/container.tsx
+++ src/app/components/container.tsx
@@ -2,10 +2,6 @@ import getClient from '@/src/client';
 import { PerspectivesDocument } from '@/src/app/perspectives.generated';
 import Presenter from './presenter';
 
-// eslint-disable-next-line max-len
-// eslint-disable-next-line @typescript-eslint/no-magic-numbers, @typescript-eslint/no-unsafe-assignment
-export const perspectives = [...Array(10)].map((_, index) => `perspective${index + 1}`);
-
 const Container = async () => {
   const { data } = await getClient().query({ query: PerspectivesDocument });
   return <Presenter perspectives={data.perspectives} />;
