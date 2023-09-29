1ed8ae989d247649cc0f69535cdd4ba2d7b7b79b
kupuma-ru21 <kupumaru21@gmail.com>
2023-09-29 17:31:51 +0900

    feat: replace modular forms with react hook form to avoid build error

--- src/app/components/presenter/components/create-perspective.tsx
+++ src/app/components/presenter/components/create-perspective.tsx
@@ -1,54 +1,63 @@
 'use client';
 
-/* eslint-disable @typescript-eslint/prefer-readonly-parameter-types */
 import {
   type Input, minLength, object, string, maxLength,
 } from 'valibot';
 import { css } from '@/styled-system/css';
-import { useForm, valiForm } from '@modular-forms/react';
-import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+import { useCallback, type SyntheticEvent } from 'react';
+import { valibotResolver } from '@hookform/resolvers/valibot';
 
-const DOM_ID = 'create-perspective';
+const DOM_ID = 'perspective';
 const MIN_LENGTH_PERSPECTIVE = 1;
 const MAX_LENGTH_PERSPECTIVE = 50;
 
+function handleSubmitHelperFunction<T>(onPromise: (event: SyntheticEvent) => Promise<T>) {
+  return (event: SyntheticEvent) => {
+    onPromise(event).catch(() => {
+      // TODO: handling
+    });
+  };
+}
+
 const Schema = object({
-  perspective: string('perspective must be a string.', [
+  [DOM_ID]: string('perspective must be a string.', [
     minLength(MIN_LENGTH_PERSPECTIVE, 'Please enter perspective.'),
     maxLength(MAX_LENGTH_PERSPECTIVE, 'Please make sure perspective is less than or 50 characters.'),
   ]),
 });
 
 const CreatePerspective = () => {
-  const [{ invalid }, { Form, Field }] = useForm<Input<typeof Schema>>(
-    { validate: valiForm(Schema) },
+  const { register, handleSubmit, formState: { errors, isValid } } = useForm<Input<typeof Schema>>(
+    {
+      resolver: valibotResolver(Schema),
+      defaultValues: { perspective: '' },
+    },
   );
-  const createPerspective = useCallback(() => {
+  const submit = useCallback(() => {
+    // TODO: handling
   }, []);
 
+  const errorInfo = errors[DOM_ID];
+
   return (
-    <Form onSubmit={createPerspective}>
-      <Field name="perspective">
-        {(field, props) => (
-          <label className={css({ fontWeight: 'bold', cursor: 'pointer' })} htmlFor={DOM_ID}>
-            create perspective
-            <input
-              {...props}
-              id={DOM_ID}
-              name={DOM_ID}
-              type="text"
-              className={css({
-                borderColor: 'Highlight', borderWidth: 'medium', borderRadius: 'md', ml: '1.5', width: 'xl',
-              })}
-            />
-            <p className={css({ color: 'red.500', mb: '1.5' })}>{field.error}</p>
-            {/* TODO: add preview ui for input */}
-          </label>
-        )}
-      </Field>
+    <form onSubmit={handleSubmitHelperFunction(handleSubmit(submit))}>
+      <label className={css({ fontWeight: 'bold', cursor: 'pointer' })} htmlFor={DOM_ID}>
+        create perspective
+        <input
+          {...register('perspective')}
+          id={DOM_ID}
+          type="text"
+          className={css({
+            borderColor: 'Highlight', borderWidth: 'medium', borderRadius: 'md', ml: '1.5', width: 'xl',
+          })}
+        />
+        {errorInfo ? <p className={css({ color: 'red.500', mb: '1.5' })}>{errorInfo.message}</p> : null}
+        {/* TODO: add preview ui for input */}
+      </label>
+      {/* TODO: fix button position degression when validation error occurs */}
       <button
-        // https://github.com/fabian-hiller/modular-forms/issues/104
-        disabled={invalid.value}
+        disabled={!isValid}
         type="submit"
         className={css({
           ml: '1.5', bgColor: 'blue.300', padding: 1, borderRadius: '8px', cursor: 'pointer',
@@ -56,7 +65,7 @@ const CreatePerspective = () => {
       >
         create
       </button>
-    </Form>
+    </form>
   );
 };
 
